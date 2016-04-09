b6c0f152b38cabc287da03d6a3957af38f3b6991
shrinivasgoudar <shrinivas.goudar@gmail.com>
2016-04-10 00:14:15 +0530

    ACD_JAVAB_Session_1_Assignment_2_Main
    ACD_JAVAB_Session_1_Assignment_2_Main

---
+++ src/com/acadgild/MultipleVariablesInSingleDeclaration.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.acadgild;
+
+/**
+ * @author shrinivasa.goudar
+ *
+ */
+public class MultipleVariablesInSingleDeclaration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Multiple Variable In Single Declaration and initialization Program!!!");
+		// Declaration of variables
+		//Datatype variable1, variable2,variable3 and initializing variable 2 with value as 4
+		int a,b=4,c;
+		
+		//Printing integer numbers
+		System.out.println("Integer B is assigned with value:"+b);
+	}
+
+}
